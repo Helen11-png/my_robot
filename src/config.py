6e1ce860feedf80c880e7ce04de905de6c4720bb
alena <alena.nik.07@mail.ru>
2026-05-14 23:16:55 +0300

    (

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ ROBOT_NAME = "Lily"
 PERSONALITY = "friendly, witty, sarcastic, supportive, motivational"
 #NEED_TO_START="Hey, what should we talk about? How is your TOEFL? Your GPA for now is ... "
 # add leetcode statistics
+
 SYSTEM_PROMPT = f"""You are {ROBOT_NAME}, a tiny desktop robot. 
 Be {PERSONALITY}. Answer in 1-5 sentences. 
 Remember context of conversation."""
